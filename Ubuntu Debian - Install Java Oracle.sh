04dca3c31d252c9c981851e2e5a12a13ef7b90fb
Luiz Fernando Hanauer <hanaure@gmail.com>
2016-07-13 08:57:16 -0300

    Delete Ubuntu Debian - Install Java Oracle.sh

--- Ubuntu Debian - Install Java Oracle.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get purge openjdk*
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-sudo apt-get install oracle-java8-set-default
