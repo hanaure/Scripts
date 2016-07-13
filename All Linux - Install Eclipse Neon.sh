7df6e2bdbacde8a4e271052767aa2657db08be94
Luiz Fernando Hanauer <hanaure@gmail.com>
2016-07-13 12:00:11 -0300

    Add files via upload

---
+++ All Linux - Install Eclipse Neon.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install Eclipse
+
+
+# Download Eclipse
+rm -f eclipse.tar.gz
+wget http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/neon/R/eclipse-jee-neon-R-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
+
+# Extraindo o Eclipse
+rm -Rf eclipse
+tar -xvf eclipse.tar.gz
+
+# Movendo Eclipse
+rm -Rf /opt/eclipse
+mv eclipse /opt/
+
+# Create Link
+rm -f /bin/eclipse
+ln -s /opt/eclipse/eclipse /bin/eclipse
+
+# Create .desktop
+rm -f /usr/share/applications/Eclipse.desktop
+echo "#!/usr/bin/env xdg-open
+[Desktop Entry]
+Name=Eclipse
+Comment=IDE de Desenvolvimento
+Exec=eclipse %U
+Icon=/opt/eclipse/icon.xpm
+MimeType=application/x-bittorrent;x-scheme-handler/magnet;
+StartupNotify=false
+Categories=Application;Development;Java;IDE;
+Type=Application" > /usr/share/applications/Eclipse.desktop
